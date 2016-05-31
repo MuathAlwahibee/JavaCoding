98b1d26ae2393267cb779be1a7e4bfe9e9d92c90
MuathAlwahibee <mo3ath.20@hotmail.com>
2016-05-30 22:12:49 -0400

    Add files via upload

---
+++ HowToPrint-In-Java.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author memo_k_96
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "Muath Alwahibee";
+         String info = "\nBorn in Saudi Arabia"; 
+         // '\n' is used to write anything in the second line
+         // but not in the same line. 
+         // to understand my point, remove '\n' and run the program.
+         System.out.println(name+info);
+
+    }
+    
+}
