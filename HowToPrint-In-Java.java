3fe0fdcaa7f3c68ae9012ed70374e7b691c4d87b
MuathAlwahibee <mo3ath.20@hotmail.com>
2016-05-30 22:13:34 -0400

    Update HowToPrint-In-Java.java

--- HowToPrint-In-Java.java
+++ HowToPrint-In-Java.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
-/**
- *
- * @author memo_k_96
- */
 public class Test {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         String name = "Muath Alwahibee";
          String info = "\nBorn in Saudi Arabia"; 
