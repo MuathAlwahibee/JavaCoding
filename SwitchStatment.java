80c01121a961970a56373cafd6bf7c16b8511de4
MuathAlwahibee <mo3ath.20@hotmail.com>
2016-05-30 21:56:18 -0400

    Add files via upload

---
+++ SwitchStatment.java
@@ -0,0 +1,28 @@
+package switchstatment;
+
+public class SwitchStatment {
+
+    public static void main(String[] args) {
+        int age;
+        age = 5;
+        switch (age) {
+            case 1:
+                System.out.println("Hay There");
+                break;
+            case 2:
+                System.out.println("Hello World");
+                break;
+            case 3:
+                System.out.println("I'm Here");
+                break;
+            case 4:
+                System.out.println("Go Home");
+                break;
+            default:
+                System.out.println("Booo");
+                break;
+            
+        }
+    }
+    
+}
